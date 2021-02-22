aacc9a17d44f220a56a6748c4d92714e957eed2e
ultrayulenka <cherevata.jul@gmail.com>
2021-02-22 19:18:56 +0200

    Fixed calc function and added Math.PI in homework#4

--- homework_4/script.js
+++ homework_4/script.js
@@ -1,10 +1,10 @@
 
 function calculateArea(radius){
-    return 3.14*radius*radius;
+    return Math.PI*radius*radius;
 }
 
 function calculateCircumference(radius) {
-    return 2 * 3.14 * radius;
+    return 2 * Math.PI * radius;
 }
 
 function checkIfNumber(value){
@@ -32,54 +32,53 @@ function calc(x, y, action){
     if(action==="+" || action==="-" ||action==="*" ||action==="/" ||action==="%" ||action==="^"){
         switch(action){
             case "+":{
-                console.log("Результат: x",action,"y =",x+y);
-                break;
+                return x+y;
             }
             case "-":{
-                console.log("Результат: x",action,"y =",x-y);
-                break;
+                return x-y;
             }
             case "*":{
-                console.log("Результат: x",action,"y =",x*y);
-                break;
+                return x*y;
             }
             case "/":{
                 if(y===0){
                     console.log("Деление на 0");
                     break;
                 }
-                console.log("Результат: x",action,"y =",x/y);
-                break;
+                return x/y;
             }
             case "%":{
                 if(y===0){
                     console.log("Деление на 0");
                     break;
                 }
-                console.log("Результат: x",action,"y =",x%y);
-                break;
+                return x%y;
             }
             case "^":{
-                console.log("Результат: x",action,"y =",Math.pow(x, y));
-                break;
+                return Math.pow(x, y);
             }
         }
     } else{
         console.log("Вы ввели неверный знак");
+        return;
     }
 }
 
 const radius=Number(prompt("Введите радиус круга в метрах"));
 if(checkIfNumber(radius) && checkIfPositive(radius)){
-    console.log("Площадь круга",calculateArea(radius),"м");
+    console.log("Площадь круга",calculateArea(radius),"м^2");
     console.log("Длина окружности",calculateCircumference(radius),"м");
 }
+
 const x=Number(prompt("Введите число х"));
 const y=Number(prompt("Введите число y"));
 if(checkIfNumber(x) && checkIfNumber(y)){
     console.log("Введеные числа - ",x,y)
     console.log("Среднее арифметическое ваших чисел",calculateAverage(x,y))
     const action=prompt("Введите математических знак +, -, *, /, %, ^");
-    calc(x, y, action);
+    const result=calc(x, y, action);
+    if(result!=undefined){
+        console.log("Результат: x",action,"y =",result);
+    }
 }
 
