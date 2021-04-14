35e3785c1c4ca39a753f50900b7f20c49ba328c0
ultrayulenka <cherevata.jul@gmail.com>
2021-04-14 22:34:46 +0300

    Added homework#15

--- homework_15/script.js
+++ homework_15/script.js
@@ -1,13 +1,18 @@
 const select = document.querySelector("select");
+const colors = ["#d45a26", "#d46c3f", "#d98764", "#d46c3f", "#d45a26"];
 let ACTIVE_INTERVAL_CONSOLE;
 let ACTIVE_INTERVAL_SCREEN;
+const wrapper = document.querySelector(".wrapper");
+createElements("HH:MM:SS",wrapper);
+elements = wrapper.children;
+ACTIVE_INTERVAL_CONSOLE = showTimeConsole(1000,getTime);
+ACTIVE_INTERVAL_SCREEN = showTimeScreen(1000,getTime,wrapper);
 
 select.addEventListener('change', e =>{
-   const wrapper = document.querySelector(".wrapper");
+   const parent = document.querySelector(".wrapper");
    clearInterval(ACTIVE_INTERVAL_CONSOLE);
    clearInterval(ACTIVE_INTERVAL_SCREEN);
-   deleteElemets(wrapper);
-   let elements = wrapper.children;
+   deleteElemets(parent);
    let timeFunction,interval,newFormat;
    switch(e.target.selectedIndex){
         case 0: {
@@ -27,15 +32,15 @@ select.addEventListener('change', e =>{
         }
    }
    newFormat = timeFunction();
-   createElements(newFormat,wrapper);
+   createElements(newFormat,parent);
    ACTIVE_INTERVAL_CONSOLE =showTimeConsole(interval,timeFunction);
-   ACTIVE_INTERVAL_SCREEN = showTimeScreen(interval,timeFunction,elements);
+   ACTIVE_INTERVAL_SCREEN = showTimeScreen(interval,timeFunction,parent);
 })
 
-function addElements(number,parent){
-    for(let i=0; i<number; i++){
+function createElements(string,parent){
+    for(let i=0; i<string.length; i++){
         const container = document.createElement("div");
-        container.style.color = colors[colors.length % i];
+        container.style.color = colors[i % colors.length];
         parent.appendChild(container);
     }
 }
@@ -46,24 +51,35 @@ function deleteElemets(parent){
     }
 }
 
+function addElements(number,parent){
+    for(let i=0; i<number; i++){
+        const container = document.createElement("div");
+        container.style.color = colors[i % colors.length];
+        parent.appendChild(container);
+    }
+}
+
+function removeElements(number,parent){
+    for(let i=0; i<number; i++){
+        parent.removeChild(parent.lastChild);
+    }
+}
+
 function getShortTime(divider){
     divider = divider? divider: ":";
     const date = new Date();
     const time = {};
     time.hours = date.getHours();
     time.minutes = date.getMinutes();
-    if(time.hours>12){
-        time.hours -= 12;
-    } else if(time.hours===0){
-        time.hours = 12;
-    }
-    for (key in time) {
-        time[key] = time[key]<10? "0"+time[key] : time[key];
-    }
     if(time.hours>=12){
         time.ampm = "PM";
     } else {
         time.ampm = "AM";
+    }
+    if(time.hours>12){
+        time.hours -= 12;
+    } else if(time.hours===0){
+        time.hours = 12;
     }
     return time.hours + divider + time.minutes + ' ' + time.ampm;
 }
@@ -100,29 +116,21 @@ function getLongTime(timeDivider,dateDivider){
 function showTimeConsole(interval,timeFunction){
     return setInterval(function(){
         console.clear();
-        console.log(timeFunction(":"));
+        console.log(timeFunction());
     },interval);
 }
 
-function createElements(string,parent){
-    var colors = ["#d45a26", "#d46c3f", "#d98764", "#d46c3f", "#d45a26"];
-    for(let i=0; i<string.length; i++){
-        const container = document.createElement("div");
-        container.style.color = colors[i % colors.length];
-        parent.appendChild(container);
-    }
-}
-
-function showTimeScreen(interval,timeFunction,containers){
+function showTimeScreen(interval,timeFunction,parent){
     return setInterval(function(){
-        const time = timeFunction(":");
+        let containers = parent.children;
+        const time = timeFunction();
+        if(time.length>containers.length){
+            addElements(time.length-containers.length,parent);
+        } else if(time.length<containers.length){
+            removeElements(containers.length-time.length,parent)
+        }
         for(let i=0; i<containers.length; i++){
             containers[i].innerText = time[i];
         }
     },interval);
 }
-const wrapper = document.querySelector(".wrapper");
-createElements("HH:MM:SS",wrapper);
-elements = wrapper.children;
-ACTIVE_INTERVAL_CONSOLE=showTimeConsole(1000,getTime);
-ACTIVE_INTERVAL_SCREEN = showTimeScreen(1000,getTime,elements);
